11026daff8bd85237d33e7f760106f692e9be320
majid Golmohammadzadeh <39524807+majidGMZ@users.noreply.github.com>
2020-02-01 11:31:57 +0330

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+class main():
+    def printHello(self):
+        print('Hello World!')
+
+    def printbranch(self):
+        print('this is second branch!')
+
+    def printhi(self):
+        print('Hi!')
+
+
+if __name__ == '__main__':
+    main.printHello()
